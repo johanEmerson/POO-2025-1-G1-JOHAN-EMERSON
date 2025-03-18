b4631dbc369f31202163a35861026cbee0d86fe8
dmamanipar <dmamanipar@unsa.edu.pe>
2025-03-18 11:16:50 -0500

    List Ejemplo

--- RepasoFP/src/Main.java
+++ RepasoFP/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,7 +8,44 @@ public class Main {
     Persona[] personas;
     Scanner sc = new Scanner(System.in);
 
+    List<Persona> listaPersonas = new ArrayList<>();
+   // ArrayList<Persona> alista= new ArrayList<>();
 
+
+    public void registrarAsisTeciaList(){
+        String existeP = "SI";
+        while(existeP.equals("SI")){
+            System.out.println("Ingrese el Nombre del Persona ");
+            String nombre = sc.nextLine();
+            System.out.println("Ingrese el Estado del Persona ");
+            String estado = sc.nextLine();
+            Persona persona = new Persona();
+            persona.setNombre(nombre);
+            persona.setEstado(estado);
+            listaPersonas.add(persona);
+            System.out.println("Existe mas estudiantes matriculados (SI/NO)");
+            existeP = sc.nextLine().toUpperCase();
+        }
+
+        for(Persona persona : listaPersonas){
+            System.out.println(persona.getNombre() + "\t" + persona.getEstado());
+        }
+
+        System.out.println("Ingrese el nombre del Persona :");
+        String nombre = sc.nextLine();
+        for(Persona persona : listaPersonas){
+            if(persona.getNombre().equals(nombre)){
+                persona.setEstado("J");
+            }
+            listaPersonas.set(listaPersonas.indexOf(persona), persona);
+        }
+
+        for(Persona persona : listaPersonas){
+            System.out.println(persona.getNombre() + "\t" + persona.getEstado());
+        }
+
+
+    }
     public void registrarAsistencia() {
         System.out.println("Ingrese la cantidad de estudiantes matriculados");
         int cantidad = sc.nextInt();
@@ -29,6 +68,7 @@ public class Main {
     public static void main(String[] args) {
         // new Main().registrarAsistencia();
         Main main = new Main();
-        main.registrarAsistencia();
+        //main.registrarAsistencia();
+        main.registrarAsisTeciaList();
     }
 }
