1791940df34aaaeaacbca5f4ca259c056dc3f5f0
dmamanipar <dmamanipar@unsa.edu.pe>
2025-03-21 10:20:31 -0500

    Basico Listas

--- RepasoFP/src/Persona.java
+++ RepasoFP/src/Persona.java
@@ -2,6 +2,15 @@ public class Persona {
     public String nombre;
     public String estado;
 
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+    public Persona(String nombre, String estado) {
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+    public Persona(){}
+
     public String getNombre() {
         return nombre;
     }
