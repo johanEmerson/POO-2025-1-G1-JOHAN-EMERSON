e4830353c5aade837900dd47035d14c8c7ac95a1
dmamanipar <dmamanipar@unsa.edu.pe>
2025-03-21 11:04:51 -0500

    Ejemplos

---
+++ SysAsisGUI/src/main/java/pe/edu/upeu/sysasisgui/SysAsisGuiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysasisgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAsisGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysAsisGuiApplication.class, args);
+	}
+
+}
